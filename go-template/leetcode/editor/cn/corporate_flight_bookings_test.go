e785ffd9a0d611c09a67b1f45aa8a0bb338eae64
hekai000 <675974119@qq.com>
2025-04-02 16:37:24 +0800

    hk commit feature)差分数组习题

---
+++ go-template/leetcode/editor/cn/corporate_flight_bookings_test.go
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=1109 lang=golang
+ * @lcpr version=30104
+ *
+ * [1109] 航班预订统计
+ */
+
+package leetcode_solutions
+
+import "testing"
+
+// @lc code=start
+
+// 差分数组工具类
+type Difference struct {
+	// 差分数组
+	diff []int
+}
+
+// 输入一个初始数组，区间操作将在这个数组上进行
+func NewDifference(nums []int) *Difference {
+	diff := make([]int, len(nums))
+	// 根据初始数组构造差分数组
+	diff[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		diff[i] = nums[i] - nums[i-1]
+	}
+	return &Difference{diff: diff}
+}
+
+// 给闭区间 [i, j] 增加 val（可以是负数）
+func (d *Difference) Increment(i, j, val int) {
+	d.diff[i] += val
+	if j+1 < len(d.diff) {
+		d.diff[j+1] -= val
+	}
+}
+
+// 返回结果数组
+func (d *Difference) Result() []int {
+	res := make([]int, len(d.diff))
+	// 根据差分数组构造结果数组
+	res[0] = d.diff[0]
+	for i := 1; i < len(d.diff); i++ {
+		res[i] = res[i-1] + d.diff[i]
+	}
+	return res
+}
+
+func corpFlightBookings(bookings [][]int, n int) []int {
+	nums := make([]int, n)
+	df := NewDifference(nums)
+
+	for _, booking := range bookings {
+		i := booking[0] - 1
+		j := booking[1] - 1
+		val := booking[2]
+		df.Increment(i, j, val)
+	}
+	return df.Result()
+}
+
+// @lc code=end
+
+func TestCorporateFlightBookings(t *testing.T) {
+	// your test code here
+
+}
+
+/*
+// @lcpr case=start
+// [[1,2,10],[2,3,20],[2,5,25]]\n5\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[1,2,10],[2,2,15]]\n2\n
+// @lcpr case=end
+
+*/
