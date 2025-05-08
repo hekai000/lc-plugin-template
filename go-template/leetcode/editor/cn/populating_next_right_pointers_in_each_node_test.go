affb0e74b516d91080eea7c2c953853b76189365
hekai000 <675974119@qq.com>
2025-05-08 14:02:05 +0800

    hk commit feature)add 116 lc

---
+++ go-template/leetcode/editor/cn/populating_next_right_pointers_in_each_node_test.go
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=116 lang=golang
+ * @lcpr version=30104
+ *
+ * [116] 填充每个节点的下一个右侧节点指针
+ */
+
+package leetcode_solutions
+
+import "testing"
+
+
+// @lc code=start
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+
+func connect(root *Node) *Node {
+	
+}
+// @lc code=end
+
+func TestPopulatingNextRightPointersInEachNode(t *testing.T) {
+	// your test code here
+	
+}
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5,6,7]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
+
